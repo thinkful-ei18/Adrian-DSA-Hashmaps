18d7cce2ab5d8fbd232cf66011b4e194eda43856
Adrian <adriantoddross@gmail.com>
2018-04-09 11:49:56 -0400

    add LOR characters

--- lor.js
+++ lor.js
@@ -5,4 +5,17 @@ const Hashmap = require('./hashmap');
 const main = (function () {
   const lor = new Hashmap();
 
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Gandalf', 'Wizard');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Ringbearer', 'Gollum');
+  lor.set('Lady of Light', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+
+
+
 });
