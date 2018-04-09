54ed937982fbd041974d35cbe29770fa9c5c6117
Adrian <adriantoddross@gmail.com>
2018-04-09 15:31:30 -0400

    capi help: finish palindrome drill

--- palindrome.js
+++ palindrome.js
@@ -2,7 +2,41 @@
 const Hashmap = require('./hashmap');
 
 function main () {
-  const lor = new Hashmap();
+
+  function uniqueChars (string) {
+    const chars = [];
+    for (let i = 0; i < string.length; i++) {
+      if (!(chars.includes(string[i]))) {
+        chars.push(string[i]);
+      }
+    }
+    return chars;
+  }
+
+  function palindrome(string) {
+    const chars = uniqueChars(string);
+    const hash = new Hashmap();
+    let count = 0;
+
+
+    for (let i = 0; i < chars.length; i++) {
+      hash.set(chars[i], 0);
+    }
+
+    for (let j = 0; j < string.length; j++) {
+      hash.set(string[j], (hash.get(string[j]) + 1));
+    }
+
+    for (let k = 0; k < chars.length; k++) {
+      if (hash.get(chars[k]) % 2 !== 0) {
+        count++;
+      }
+    }
+
+    console.log(hash);
+    console.log(count < 2);
+  }
+  palindrome('racecar');
 }
 
 main();
